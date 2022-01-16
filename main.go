237ee3a490170f83f7ddcca086a4fa024df35839
Yurino Oga <yurino.o@latona.io>
2022-01-16 18:19:24 +0900

    release

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	sap_api_caller "sap-api-integrations-inspection-plan-reads/SAP_API_Caller"
+	"sap-api-integrations-inspection-plan-reads/sap_api_input_reader"
+
+	"github.com/latonaio/golang-logging-library/logger"
+)
+
+func main() {
+	l := logger.NewLogger()
+	fr := sap_api_input_reader.NewFileReader()
+	inoutSDC := fr.ReadSDC("./Inputs/SDC_Inspection_Plan_Inspection_Specification_sample.json")
+	caller := sap_api_caller.NewSAPAPICaller(
+		"https://sandbox.api.sap.com/s4hanacloud/sap/opu/odata/sap/", l,
+	)
+
+	accepter := inoutSDC.Accepter
+	if len(accepter) == 0 || accepter[0] == "All" {
+		accepter = []string{
+			"Header", "MaterialAssignment", "Operation",
+			"BillOfOperationsDesc", "InspectionSpecification",
+		}
+	}
+
+	caller.AsyncGetInspectionPlan(
+		inoutSDC.InspectionPlan.InspectionPlanGroup,
+		inoutSDC.InspectionPlan.InspectionPlan,
+		inoutSDC.InspectionPlan.Plant,
+		inoutSDC.InspectionPlan.MaterialAssignment.Material,
+		inoutSDC.InspectionPlan.BillOfOperationsDesc,
+		inoutSDC.InspectionPlan.Operation.InspectionSpecification,
+		accepter,
+	)
+}
